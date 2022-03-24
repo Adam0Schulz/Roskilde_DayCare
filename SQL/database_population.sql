8c33acdb9c22a51480c10e2a42a964ebef51762e
simonatingleffkardel <simonakardel@gmail.com>
2022-03-24 08:32:53 +0100

    database population

--- SQL/database_population.sql
+++ SQL/database_population.sql
@@ -28,6 +28,28 @@ INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_c
 VALUES ('Lamar', 'Hudson', 'hudson@hotmail.com', 'Hersnapvej 25', 1098, 'Copenhagen', '9568748');
 INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
 VALUES ('Leon', 'Padilla', 'padilla@dgmail.com', 'Jomfrustien 62', 5631, 'Ebberup', '39845738');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Cody', 'Wells', 'wells@gmail.com', 'Østerøvej 23', 2600, 'Glostrup', '74838383');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Jimmie', 'Huff', 'huff@gmail.com', 'Skolegade 80', 1660, 'Copenhagen', '37374883');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Pam', 'Summers', 'summers@gmail.com', 'Ladbyvej 10', 2500, 'Valby', '74394893');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Raul', 'Mcdonald', 'mcdonald@gmail.com', 'Søndergade 55', 2235, 'Copenhagen', '83847383');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Søren', 'Petersen', 'petersen@gmail.com', 'Møllebakken 33', 1100, 'Copenhagen', '18383833');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Lydia', 'Gonzales', 'gonzales@gmail.com', 'Hersnapvej 25', 1098, 'Copenhagen', '92947474');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Kara', 'Schultz', 'schultz@gmail.com', 'Jomfrustien 62', 5631, 'Ebberup', '98274837');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Tine', 'Stephensen', 'stephensen@gmail.com', 'Søndergade 55', 2235, 'Copenhagen', '82646738');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Nicolas', 'Lynch', 'lynch@gmail.com', 'Møllebakken 33', 1100, 'Copenhagen', '28474838');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Gerald', 'Kim', 'kim@hotmail.com', 'Hersnapvej 25', 1098, 'Copenhagen', '93748374');
+INSERT INTO Daycare.Parents (first_name, last_name, email_address, street, zip_code, city, phone_number)
+VALUES ('Leslie', 'Sanders', 'sanders@dgmail.com', 'Jomfrustien 62', 5631, 'Ebberup', '93473874');
 
 INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Mathilde', 'Weasley', 1, '2019-09-02', '838938933', 'F');
 INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Alejandro', 'Russell', 2, '2019-08-02', '83873478', 'M');
@@ -44,6 +66,138 @@ INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, c
 INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Penelope', 'Doyle', 13, '2020-07-02', '6372727272', 'F');
 INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Theodore', 'Frank', 14, '2020-08-02', '0838383883', 'M');
 INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Zeleste', 'Kardel', 15, '2020-09-02', '3737377333', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Aya', 'Wells', 16, '2019-06-02', '837478373', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Monika', 'Huff', 17, '2021-03-02', '352737273', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Simon', 'Summers', 18, '2020-05-02', '765848458', 'M');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Barbara', 'Mcdonald', 19, '2021-01-02', '73737373', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Amalie', 'Petersen', 20, '2021-06-02', '836483673', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Veronica', 'Gonzales', 21, '2020-10-05', '483764837', 'F');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Albert', 'Schultz', 22, '2021-02-15', '136364733', 'M');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Vincent', 'Stephensen', 23, '2020-01-02', '0378434444', 'M');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('John', 'Lynch', 24, '2019-01-02', '83748378', 'M');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Joe', 'Kim', 25, '2020-05-02', '84783744', 'M');
+INSERT INTO Daycare.Children (first_name, last_name, parent_ID, date_of_birth, cpr, gender) VALUES ('Meredith', 'Sanders', 26, '2020-07-02', '87374874', 'F');
+
+
+INSERT INTO Daycare.Classes (name) VALUES ('Hedgehogs');
+INSERT INTO Daycare.Classes (name) VALUES ('Foxes');
+INSERT INTO Daycare.Classes (name) VALUES ('Bees');
+
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (1, 1);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (2, 1);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (3, 1);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (4, 1);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (5, 1);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (6, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (7, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (8, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (9, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (10, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (11, 2);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (12, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (13, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (14, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (15, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (16, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (17, 3);
+INSERT INTO Daycare.Attendees (child_ID, group_ID) VALUES (18, 1);
+
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (19);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (20);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (21);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (22);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (23);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (24);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (25);
+INSERT INTO Daycare.Waiting_list (Child_ID) VALUES (26);
+
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Sandra', 'Madsen', 'smadsen@admin.dk', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '87373744', 35000, '64387483', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Kathrine', 'Lund', 'lund@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '327637263', 26000, '837483744', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Line', 'Buhl', 'buhl@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '73647373', 26000, '53536464', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Stine', 'Elkjær', 'elkjaer@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '37837373', 26000, '88585747', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Tove', 'Brøndum', 'toveb@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '3737373', 26000, '029828444', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Tilde', 'Nissen', 'nissen@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '38383883', 26000, '72637263', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Bo', 'Haugaard', 'haugaard@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '784874387', 26000, '95494848', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Kristian', 'Torp', 'torp@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '37373733', 26000, '163636333', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Elo', 'Birch', 'birch@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '47437373', 24000, '736473673', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Diana', 'Simonsen', 'simonsen@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '87387387', 24000, '983748373', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Liva', 'Gram', 'gram@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '73737373', 24000, '937483738', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Rikke', 'Vinther', 'vinther@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '85685858', 20000, '73647363', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Pernille', 'Busk', 'busk@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '57858745', 20000, '7364763', 'roskilde');
+INSERT INTO Daycare.Employees (first_name, last_name, email_address, street, zip_code, city, phone_number, salary, CPR, password) VALUES ('Nelly', 'Berthelsen', 'berthelsen@gmail.com', 'Gertrude Steins Vej 22', 2300, 'Copenhagen S', '848484433', 20000, '736476333', 'roskilde');
+
+INSERT INTO Daycare.Schedule (active) VALUES (true);
+
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 1, 1, '06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 2, 1, '06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 9, 3, '06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 10, 3,'06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 5, 2, '06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 6, 2, '06:00:00', '12:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 3, 1, '12:00:00', '17:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 4, 1, '12:00:00', '17:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 7, 2, '12:00:00', '17:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 8, 2, '12:00:00', '17:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 11, 3,'12:00:00', '17:00:00', 'MO');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 12, 3,'12:00:00', '17:00:00', 'MO');
+
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 3, 1, '06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 4, 1, '06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 11, 3,'06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 12, 3,'06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 5, 2, '06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 6, 2, '06:00:00', '12:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 1, 1, '12:00:00', '17:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 2, 1, '12:00:00', '17:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 7, 2, '12:00:00', '17:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 8, 2, '12:00:00', '17:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 10, 3,'12:00:00', '17:00:00', 'TU');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 9, 3, '12:00:00', '17:00:00', 'TU');
+
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 3, 1, '06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 4, 1, '06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 11, 3,'06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 12, 3,'06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 5, 2, '06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 6, 2, '06:00:00', '12:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 1, 1, '12:00:00', '17:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 2, 1, '12:00:00', '17:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 7, 2, '12:00:00', '17:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 8, 2, '12:00:00', '17:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 10, 3,'12:00:00', '17:00:00', 'WE');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 9, 3, '12:00:00', '17:00:00', 'WE');
+
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 1, 1, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 2, 1, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 5, 2, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 6, 2, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 9, 3, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 10, 3, '06:00:00', '12:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 3, 1, '12:00:00', '17:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 4, 1, '12:00:00', '17:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 7, 2, '12:00:00', '17:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 8, 2, '12:00:00', '17:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 11, 3,'12:00:00', '17:00:00', 'TH');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 12, 3,'12:00:00', '17:00:00', 'TH');
+
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 13, 1, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 2, 1, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 3, 1, '12:00:00', '17:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 4, 1, '12:00:00', '17:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 5, 2, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 14, 2, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 7, 2, '12:00:00', '17:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 8, 2, '12:00:00', '17:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 9, 3, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 10, 3, '06:00:00', '12:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 11, 3, '12:00:00', '17:00:00', 'FR');
+INSERT INTO Employee_assigment (schedule_ID, employee_ID, group_ID, start_time, end_time, day) VALUES (1, 12, 3, '12:00:00', '17:00:00', 'FR');
+
+
+
+
+
+
 
 
 
